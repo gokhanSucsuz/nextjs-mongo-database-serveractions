d165d2ab5a0ac87ec052a660e7f7ecf8cefac25d
Gökhan SUÇSUZ <coolpisces22@gmail.com>
2024-06-21 19:28:23 +0300

    db change

--- app/actions/addUserAction.ts
+++ app/actions/addUserAction.ts
@@ -3,6 +3,14 @@ import DbHelper from "../utils/dbHelpers";
 import { Collection } from "mongodb";
 import { redirect } from "next/navigation";
 
+import { z } from "zod";
+
+const formSchema = z.object({
+	email: z.string().email("Please enter a valid email!"),
+	password: z.string().min(8, "Please enter at least 8 character!"),
+	userName: z.string()
+});
+
 export async function addUser(formData: FormData) {
 	const userName = formData.get("userName");
 	const email = formData.get("email");
@@ -14,9 +22,15 @@ export async function addUser(formData: FormData) {
 		email,
 		password
 	};
-	const response = await collection.insertOne(obj);
-	if (response.insertedId) {
-		redirect("/users");
+	try {
+		formSchema.parse({ email, password, userName });
+		const response = await collection.insertOne(obj);
+		if (response.insertedId) {
+			console.log("merhaba");
+		}
+	} catch (error) {
+		console.log(error.errmsg);
+		return error.errmsg;
 	}
-	return response;
+	redirect("/users");
 }
--- app/createuser/page.jsx
+++ app/createuser/page.jsx
@@ -2,10 +2,10 @@
 import React, { useState } from 'react'
 import { addUser } from '../actions/addUserAction'
 import Link from 'next/link'
-import { CiCircleRemove } from 'react-icons/ci'
 
 const CreateUser = () => {
     const [adding, setAdding] = useState(false)
+    const [isError, setIsError] = useState(false)
     const handleClick = async () => {
         setAdding(true)
         setTimeout(() => {
@@ -14,9 +14,21 @@ const CreateUser = () => {
 
     }
 
+    const handleAction = async (formData) => {
+        const res = await addUser(formData)
+
+        if (res) {
+            console.log(res)
+            setIsError(res)
+        }
+        else {
+            setIsError(false)
+        }
+    }
+
     return (
         <div className=' p-10 border max-w-lg mx-auto my-20 shadow-lg rounded-lg'>
-            <form className="max-w-sm mx-auto" action={addUser}>
+            <form className="max-w-sm mx-auto" action={handleAction}>
                 <div className="mb-5">
                     <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your email</label>
                     <input name='email' type="email" id="email" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" placeholder="name@mail.com" required />
@@ -30,12 +42,14 @@ const CreateUser = () => {
                     <input name='password' type="password" id="password" className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 dark:shadow-sm-light" required />
                 </div>
                 <div className='flex justify-between'>
-                    <button disabled={adding} onClick={handleClick} type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save</button>
+                    <button onClick={handleClick} type="submit" className="text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Save</button>
                     <Link href="/users" className='bg-red-500 hover:bg-red-700 text-white p-2.5 rounded-md mx-2'>Cancel</Link>
                 </div>
             </form>
             <div
-                className={`bg-green-500 hover:bg-green-700 text-white font-bold my-4 p-2 rounded-md ${adding ? "" : "hidden"}`}> New User Saving...</div>
+                className={`bg-green-500 text-white hover:bg-green-300 hover:text-slate-300 font-bold my-4 p-2 rounded-md ${adding ? "" : "hidden"}`}> New User Saving...</div>
+            <div
+                className={`bg-red-500 text-white font-bold my-4 p-2 rounded-md ${isError ? "" : "hidden"}`}> This user email is already registered! Please enter new email! </div>
         </div>
 
     )
